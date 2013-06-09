cca81e54572e0ca91d14745c2704e6eadeff3d86
Romain Deterre <romain.deterre@gmail.com>
2013-06-09 19:01:42 -0400

    Initial Commit

---
+++ LightControllerMacClient/LightController.h
@@ -0,0 +1,15 @@
+//
+//  LightController.h
+//  Popup
+//
+//  Created by Romain Deterre on 2013-06-05.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LightController : NSObject
+
+- (IBAction)lightChanged:(id)sender;
+
+@end
---
+++ LightControllerMacClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LightControllerMacClient
+//
+//  Created by Romain Deterre on 2013-06-09.
+//  Copyright (c) 2013 Romain Deterre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
